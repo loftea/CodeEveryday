8e53b3997b5a0a4a2e4d5d7d41ba179363c267da
loftea <57883305+loftea@users.noreply.github.com>
2022-04-14 16:10:02 +0800

    4/14

---
+++ 二叉树/P110平衡二叉树.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+	if height(root) == -1 {
+		return false
+	}
+	return true
+}
+
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := height(root.Left)
+	right := height(root.Right)
+	if (left == -1) ||
+		(right == -1) ||
+		(abs(left-right) > 1) {
+		return -1
+	}
+	return max(left, right) + 1
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
