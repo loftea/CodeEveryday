52d62670be9ce2bad36423e092499c9d59c4b84f
loftea <57883305+loftea@users.noreply.github.com>
2022-04-19 09:50:32 +0800

    4/18

--- Hash Table/P3_longest_substring_without_repeating_characters.go
+++ Hash Table/P3_longest_substring_without_repeating_characters.go
@@ -13,20 +13,13 @@ func lengthOfLongestSubstring(s string) int {
 		currByte := int(s[index])
 
 		if LeftIndex < H[currByte] {
-			// 如果 H[currByte] 都没有被更新过，但是仍然有 LeftIndex <= H[currByte] 成立，
-			// 不应该更新，因为这个时候 0<=0 成立，
-			// 但是 LeftIndex 不应该变成 0+1=1，而仍然应该是 0
+			// 更新窗口
 			LeftIndex = H[currByte]
-			sLength = index - LeftIndex + 1
-			result = max(sLength, result)
-			H[currByte] = index + 1
-			continue
 		}
 
-		sLength++
-		result = max(result, sLength)
+		sLength = index - LeftIndex + 1
+		result = max(sLength, result)
 		H[currByte] = index + 1
-		continue
 
 	}
 
---
+++ Hash Table/P992_subarraysWithKDistinct.go
@@ -0,0 +1,39 @@
+package leetcode
+
+// import "fmt"
+
+func subarraysWithKDistinct(A []int, k int) int {
+	left, mid, right, count, res, H := 0, 0, 0, 0, 0, map[int]int{}
+	length := len(A)
+	for ; right < length; right++ {
+		x := A[right]
+
+		if H[x] <= left { // x not in window
+			count++
+		} else if H[x] == mid+1 {
+			mid++
+			for mid+1 < H[A[mid]] {
+				mid++
+			}
+		}
+
+		switch count {
+		case k + 1:
+			left = mid + 1
+			mid++
+			for mid+1 < H[A[mid]] {
+				mid++
+			}
+			count--
+			fallthrough
+
+		case k:
+			res += mid - left + 1
+		}
+
+		H[x] = right + 1
+		// fmt.Println(left,mid,right)
+	}
+
+	return res
+}
